48a4ac7bc21663d937e14be99edf12caa1e14497
AymenSec <4ymansec@gmail.com>
2024-12-29 19:00:43 +0100

    create file farasha python

---
+++ farasha.py
@@ -0,0 +1,12 @@
+#"""####################################################################################################"""
+####copyright by AymanSec this tool is free and open-source, farasha is automation tool for scaning web ###
+#"""####################################################################################################"""
+
+
+
+
+
+import requests
+import threading
+import argparse
+
